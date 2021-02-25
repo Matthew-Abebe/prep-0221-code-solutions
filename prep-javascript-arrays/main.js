96566a3b31a4cfd38a8c1c1706f276dfe0391e58
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-02-24 17:23:42 -0800

    Moved var lastItem lower on main.js

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -1,18 +1,25 @@
 var languagesArray = ['html', 'css', 'javascript', 'react', 'node.js'];
-var thirdElement = languagesArray[2];
-var arrayLength = languagesArray.length;
-var lastItem = languagesArray[languagesArray.length - 1];
-
 console.log('languages array', languagesArray);
-console.log('array length', arrayLength);
+
+console.log('array length', languagesArray.length);
+
 languagesArray.push('C#', 'php');
 console.log('updated array', languagesArray);
+
 languagesArray.pop();
 console.log('last index removed', languagesArray);
+
 languagesArray.unshift('C++');
 console.log('added element to front', languagesArray);
+
 languagesArray.shift();
 console.log('removed first element', languagesArray);
+
+var thirdElement = languagesArray[2];
 console.log('third element in languagesArray ', thirdElement);
-console.log('the final length of languagesArray ', languagesArray.length);
-console.log('the last item in the array is ', lastItem); // returning 'node.js' and not 'C#'
+
+var arrayLength = languagesArray.length;
+console.log('the final length of languagesArray ', arrayLength);
+
+var lastItem = languagesArray[arrayLength - 1];
+console.log('the last item in the array is ', lastItem);
