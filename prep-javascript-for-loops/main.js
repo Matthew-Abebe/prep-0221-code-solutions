793070b3668249632e59afedfc6e047075573a4a
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-02-26 11:56:32 -0800

    removed comments

--- prep-javascript-for-loops/main.js
+++ prep-javascript-for-loops/main.js
@@ -1,11 +1,9 @@
 // create your loops here.
 
-// This loop works.
 for (var i = 0; i < 10; i++) {
   console.log(i);
 }
 
-// This loop stops at 8 but should go on to 18.
 for (var j = 0; j <= 20; j += 2) {
   console.log(j);
 }
